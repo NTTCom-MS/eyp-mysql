2e7a82874c8d8e19fcae852f1375d7fea6b76518
Jordi Prats <jprats@systemadmin.es>
2016-09-20 15:35:59 +0200

    canvio cond

--- manifests/install.pp
+++ manifests/install.pp
@@ -40,7 +40,7 @@ class mysql::install inherits mysql {
             # echo "mysql-apt-config mysql-apt-config/select-server select mysql-5.6" | debconf-set-selections
             exec { "debian set mysql ${mysql::version}":
               command => "bash -c 'echo \"mysql-apt-config mysql-apt-config/select-server select mysql-${mysql::version}\" | debconf-set-selections'",
-              onlyif  => "bash -c 'debconf-get-selections | grep \"mysql-apt-config/select-server\" | grep \"mysql-${mysql::version}\"'",
+              unless  => "bash -c 'debconf-get-selections | grep \"mysql-apt-config/select-server\" | grep \"mysql-${mysql::version}\"'",
             }
           }
           default: {}
