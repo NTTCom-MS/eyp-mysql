c820359bd83ce23da11edb915529b7ef39a26d0d
Jordi Prats <jprats@systemadmin.es>
2017-02-08 15:22:32 +0100

    reordre

---
+++ manifests/community/instance.pp
@@ -0,0 +1,20 @@
+define mysql::community::instance (
+                                    $password,
+                                    $instance_name     = $name,
+                                    $version           = '5.7',
+                                    $add_default_mycnf = false,
+                                  ) {
+  include ::mysql
+
+  mysql::community::install { $instance_name:
+    version  => $version,
+    password => $password,
+  }
+
+  ->
+
+  mysql::community::config { $instance_name:
+    add_default_mycnf => $add_default_mycnf,
+    require           => Class['::mysql'],
+  }
+}
