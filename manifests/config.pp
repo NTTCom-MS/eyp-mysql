4b924d3dba2b81c8aa1c1591a4ee543b0526bcbc
Jordi Prats <jprats@systemadmin.es>
2017-02-08 11:52:55 +0100

    pidfile

--- manifests/config.pp
+++ manifests/config.pp
@@ -12,10 +12,10 @@ class mysql::config inherits mysql {
     systemd::service { "mysqlcommunity@":
       description                 => 'mysql community %i',
       type                        => 'forking',
-      execstart                   => "/usr/sbin/mysqld --daemonize --pid-file=/var/run/mysqld/mysqld.pid --defaults-file /etc/mysql/%i/my.cnf",
+      execstart                   => "/usr/sbin/mysqld --daemonize --pid-file=/var/run/community%i/mysqld.pid --defaults-file /etc/mysql/%i/my.cnf",
       user                        => 'mysql',
       group                       => 'mysql',
-      pid_file                    => '/var/run/community%i/mysqld.pid'
+      pid_file                    => '/var/run/community%i/mysqld.pid',
       permissions_start_only      => true,
       restart                     => 'on-failure',
       limit_nofile                => '10000',
@@ -23,6 +23,7 @@ class mysql::config inherits mysql {
       restart_prevent_exit_status => [ '1' ],
       runtime_directory           => [ 'community%i' ],
       runtime_directory_mode      => '755',
+      restart_sec                 => '1',
     }
   }
   else
