e9f5ab686c3dc0f33d5d431e39ac2b67b65228b4
Jordi Prats <jprats@systemadmin.es>
2017-02-13 14:33:48 +0100

    execopath

--- manifests/community/install.pp
+++ manifests/community/install.pp
@@ -4,6 +4,10 @@ define mysql::community::install(
                                   $relaylogdir   = "/var/mysql/${name}/binlogs",
                                   $logdir        = "/var/log/mysql/${name}",
                                 ) {
+  Exec {
+    path => '/usr/sbin:/usr/bin:/sbin:/bin',
+  }
+
   exec { "mkdir datadir ${instance_name}":
     command => "mkdir -p ${datadir}",
     creates => $datadir,
