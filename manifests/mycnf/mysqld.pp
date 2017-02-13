7a761602f0ec99d682ca5169d678ced3326ee5ba
Jordi Prats <jprats@systemadmin.es>
2017-02-13 10:41:49 +0100

    my.cnf fins a binlogs

--- manifests/mycnf/mysqld.pp
+++ manifests/mycnf/mysqld.pp
@@ -1,14 +1,28 @@
 # notes:
 # tmp_table_size=max_heap_table_size
 define mysql::mycnf::mysqld (
-                              $instance_name          = $name,
-                              $skip_external_locking  = $mysql::params::skip_external_locking_default,
-                              $tmpdir                 = $mysql::params::tmpdir_default,
-                              $datadir                = "/var/mysql/${name}",
-                              $default_storage_engine = 'InnoDB',
-                              $ignoreclientcharset    = true,
-                              $charset                = 'utf8',
-                              $readonly               = false,
+                              $instance_name                   = $name,
+                              $skip_external_locking           = $mysql::params::skip_external_locking_default,
+                              $tmpdir                          = $mysql::params::tmpdir_default,
+                              $datadir                         = "/var/mysql/${name}",
+                              $default_storage_engine          = 'InnoDB',
+                              $ignoreclientcharset             = true,
+                              $charset                         = 'utf8',
+                              $readonly                        = false,
+                              $key_buffer_size                 = $mysql::params::key_buffer_size_default,
+                              $sysdate_is_now                  = true,
+                              $max_allowed_packet              = '16M',
+                              $max_connect_errors              = '1000000',
+                              $nameresolve                     = false,
+                              $myisam_recover                  = [ 'FORCE', 'BACKUP' ],
+                              $innodb                          = 'FORCE',
+                              $binlogdir                       = $mysql::params::binlogdir_default,
+                              $expirelogsdays                  = '5',
+                              $binlog_format                   = 'MIXED',
+                              $sync_binlog                     = true,
+                              $serverid                        = '1',
+                              $max_binlog_size                 = '1073741824',
+                              $log_bin_trust_function_creators = false,
                             ) {
   if($instance_name=='global')
   {
@@ -41,5 +55,29 @@ define mysql::mycnf::mysqld (
     target  => "/etc/mysql/${instance_name}/my.cnf",
     order   => '103',
     content => template("${module_name}/mycnf/mysqld/03_rw.erb"),
+  }
+
+  concat::fragment{ "/etc/mysql/${instance_name}/my.cnf mysqld myisam":
+    target  => "/etc/mysql/${instance_name}/my.cnf",
+    order   => '104',
+    content => template("${module_name}/mycnf/mysqld/04_myisam.erb"),
+  }
+
+  concat::fragment{ "/etc/mysql/${instance_name}/my.cnf mysqld safety":
+    target  => "/etc/mysql/${instance_name}/my.cnf",
+    order   => '105',
+    content => template("${module_name}/mycnf/mysqld/05_safety.erb"),
+  }
+
+  concat::fragment{ "/etc/mysql/${instance_name}/my.cnf mysqld data storage":
+    target  => "/etc/mysql/${instance_name}/my.cnf",
+    order   => '106',
+    content => template("${module_name}/mycnf/mysqld/06_data_storage.erb"),
+  }
+
+  concat::fragment{ "/etc/mysql/${instance_name}/my.cnf mysqld binlogs":
+    target  => "/etc/mysql/${instance_name}/my.cnf",
+    order   => '107',
+    content => template("${module_name}/mycnf/mysqld/07_binlogs.erb"),
   }
 }
