3626de04b41625cc8d27d6739e87524b6b537be9
Jordi Prats <jprats@systemadmin.es>
2017-02-16 13:46:15 +0100

    canvi mysql database a resource

---
+++ lib/puppet/provider/mysql_sql/mysql_database.rb
@@ -0,0 +1,56 @@
+Puppet::Type.type(:mysql_sql).provide(:mysql_sql) do
+
+  def self.instances
+    self.run_sql_command('show databases').split("\n").collect do |db|
+      new(
+        :ensure => :present,
+        :name => db,
+        )
+    end
+  end
+
+  def self.prefetch(resources)
+    dbs=instances
+    resources.keys.each do |name|
+      if provider = dbs.find{ |db| db.name == name }
+        resources[name].provider = provider
+      end
+    end
+  end
+
+  def exists?
+    @property_hash[:ensure] == :present || false
+  end
+
+  mk_resource_methods
+
+  def create
+    self.run_sql_command(("create database " + resource[:name] + " CHARACTER SET " + resource[:charset] + " COLLATE " + resource[:collate] + ";")
+  end
+
+  def destroy
+    self.run_sql_command("drop database " + resource[:name])
+  end
+
+
+  def run_sql_command(sql)
+
+    # mysql --defaults-group-suffix=slave
+
+    command = [resource[:mysql_path]]
+    command.push("--defaults-group-suffix=" + resource[:instance_name]) if resource[:instance_name]
+    command.push("-S", resource[:socket]) if resource[:socket]
+    command.push("-p" + resource[:password]) if resource[:password]
+    command.push("-e", '"' + sql.gsub('"', '\"') + '"')
+    command.push(resource[:db]) if resource[:db]
+
+    if resource[:cwd]
+      Dir.chdir resource[:cwd] do
+        run_command(command)
+      end
+    else
+      run_command(command)
+    end
+  end
+
+end
---
+++ lib/puppet/type/mysql_database.rb
@@ -0,0 +1,50 @@
+Puppet::Type.newtype(:mysql_sql) do
+  @doc = 'manage MySQL databases'
+
+  newparam(:database) do
+    desc 'The name of the MySQL DB to manage.'
+
+    defaultto { @resource[:name] }
+
+    validate do |value|
+      unless value.is_a?(String)
+        raise Pupper::Error,
+          "not a string, modafuca"
+      end
+    end
+
+    def sync
+      output, status = provider.run_sql_command(value)
+      self.fail("Error executing SQL; mysql returned #{status}: '#{output}'") unless status == 0
+    end
+  end
+
+  newparam(:mysql_path) do
+    desc "The path to mysql executable."
+    defaultto("mysql")
+  end
+
+  newparam(:instance_name) do
+    desc "instance name to connect to"
+  end
+
+  newparam(:password) do
+    desc "password to connect"
+  end
+
+  newparam(:socket) do
+    desc "The name of the socked to use"
+  end
+
+  newparam(:charset) do
+    desc "database's default charset"
+    defaultto("utf8")
+  end
+
+  newparam(:collate) do
+    desc "database's default collate"
+    defaultto("utf8_general_ci")
+  end
+
+
+end
