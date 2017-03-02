3ab47abf22892d70bd0a3c91376929a8a65c0158
Jordi Prats <jprats@systemadmin.es>
2017-03-02 14:15:03 +0100

    xtradbcluster

--- manifests/xtradbcluster.pp
+++ manifests/xtradbcluster.pp
@@ -4,12 +4,15 @@ class mysql::xtradbcluster(
                             $version       = '5.7',
                           ) inherits mysql::params {
   #
+  include ::mysql
+
   include ::mysql::perconarepo
 
   include ::mysql::backup::xtrabackup::install
 
   package { $::mysql::percona_xtradbcluster_package_name[$version]:
-    ensure => $mysql::package_ensure,
+    ensure  => $mysql::package_ensure,
     require => Class[ [ '::mysql::perconarepo', '::mysql::backup::xtrabackup::install' ] ],
+    before  => Service[$mysql::params::servicename],
   }
 }
