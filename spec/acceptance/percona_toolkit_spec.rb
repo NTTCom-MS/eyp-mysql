2154bcdf864e3ced61d49132f4bd1c840566a86b
Jordi Prats <jprats@systemadmin.es>
2017-04-04 13:22:08 +0200

    acceptance query digest

---
+++ spec/acceptance/percona_toolkit_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper_acceptance'
+
+describe 'percona toolkit class' do
+
+  context 'basic setup ' do
+    # Using puppet_apply as a helper
+    it 'should work with no errors' do
+      pp = <<-EOF
+
+      class { 'mysql::tools::perconatoolkit': }
+
+      EOF
+
+      # Run it twice and test for idempotency
+      expect(apply_manifest(pp).exit_code).to_not eq(1)
+      expect(apply_manifest(pp).exit_code).to eq(0)
+
+    end
+
+    it "check pt-query-digest" do
+      expect(shell("echo | pt-query-digest").exit_code).to be_zero
+    end
+
+  end
+
+end
+
+#echo | pt-query-digest
