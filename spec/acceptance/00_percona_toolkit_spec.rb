4eb3ed034a3bd53ec48c407db2885bd19d4f5454
Jordi Prats <jprats@systemadmin.es>
2017-11-21 13:45:46 +0100

    fix perconatoolkit incompatible dependencies by reordering it

--- spec/acceptance/00_percona_toolkit_spec.rb
+++ spec/acceptance/00_percona_toolkit_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper_acceptance'
 describe 'percona toolkit class' do
 
   context 'basic setup ' do
+
     # Using puppet_apply as a helper
     it 'should work with no errors' do
       pp = <<-EOF
@@ -19,6 +20,21 @@ describe 'percona toolkit class' do
 
     it "check pt-query-digest" do
       expect(shell("echo | pt-query-digest").exit_code).to be_zero
+    end
+
+    it 'should work with no errors' do
+      pp = <<-EOF
+
+      class { 'mysql::tools::perconatoolkit':
+        ensure => 'absnet',
+      }
+
+      EOF
+
+      # Run it twice and test for idempotency
+      expect(apply_manifest(pp).exit_code).to_not eq(1)
+      expect(apply_manifest(pp).exit_code).to eq(0)
+
     end
 
   end
