a1180bd9ad0011eaf2119eeae05e574fd4e9f050
Jordi Prats <jordi.prats@gmail.com>
2018-08-23 16:22:08 +0200

    demo extraopts

---
+++ examples/mysqldump_extraopts.pp
@@ -0,0 +1,13 @@
+mysql::community::instance { 'test':
+  port              => '3306',
+  password          => 'password',
+  add_default_mycnf => true,
+  default_instance  => true,
+}
+
+->
+
+mysql::backup::mysqldump { 'test':
+  destination => '/backup',
+  extraopts => ','
+}
