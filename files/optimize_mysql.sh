5311e049d6cb39a53871175358a886ac7981dcec
Jordi Prats <jordi.prats@gmail.com>
2020-05-18 12:15:12 +0200

    fix optimize

--- files/optimize_mysql.sh
+++ files/optimize_mysql.sh
@@ -6,5 +6,5 @@ PATH="/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin"
 
 for i in $(echo "SELECT concat(table_schema,'.',TABLE_NAME) FROM information_schema.TABLES  WHERE TABLE_SCHEMA NOT IN ('information_schema','mysql','performance_schema') AND Data_free is not null and data_free > data_length/10;" | mysql -p$(cat /var/mysql/.mysql.root.pass) -sN);
 do
-	echo "optimize table $i;" | mysql -p$(cat /var/mysql/.mysql.root.pass) >/dev/null 2>&1
+	echo "optimize table $i;" | mysql "-p$(cat /var/mysql/.mysql.root.pass)" >/dev/null 2>&1
 done
